b671d8325617bc032527d0bbdbff3bfe8d3f27e8
paulflopezp-code <paulflopezp@gmail.com>
2026-08-27 17:32:28 -0500

    build 40: AppDelegate GMSServices provideAPIKey - fix iOS crash

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyC4D3nwkeJqNr8jof0l7gB__1GNs1OKe80")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
